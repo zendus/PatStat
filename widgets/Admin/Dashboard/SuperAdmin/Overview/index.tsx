c844d0a75f877f8db124b0bd21c23254bee9d82f
ChukwuemekaAgu <aguchukwuemeka3035@gmail.com>
2026-04-19 10:27:16 +0100

    commit: updated super admin overview
    	modified:   widgets/Admin/Dashboard/SuperAdmin/Overview/index.tsx

--- widgets/Admin/Dashboard/SuperAdmin/Overview/index.tsx
+++ widgets/Admin/Dashboard/SuperAdmin/Overview/index.tsx
@@ -120,7 +120,6 @@ const LineChart: React.FC = () => {
       width="100%"
       height="100%"
       preserveAspectRatio="xMidYMid meet"
-      style={{ display: "block" }}
     >
       <defs>
         <linearGradient id="areaGrad" x1="0" y1="0" x2="0" y2="1">
